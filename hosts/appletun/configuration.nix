f50f0ad72382eb8185e54a61935287c27adab642
Jeffrey Li <jeffrey.dot.li@gmail.com>
2024-10-07 20:50:22 -0400

    fix: tpm status for nixos host

--- hosts/appletun/configuration.nix
+++ hosts/appletun/configuration.nix
@@ -13,6 +13,7 @@ in {
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
+    initrd.systemd.enableTpm2 = false;
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
