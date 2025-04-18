1d0177a495db20fd27734b9e142138271050c5af
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-04-18 13:12:42 -0400

    feat: add global gitignore for envrc

--- packages/wrapper-manager/git/default.nix
+++ packages/wrapper-manager/git/default.nix
@@ -5,6 +5,10 @@
   ...
 }: let
   myGit = pkgs.gitFull;
+  globalGitignore = pkgs.writeText ".gitignore_global" (lib.fileContents ./.gitignore_global);
+  gitConfigContent =
+    lib.strings.replaceStrings ["$GLOBAL_GITIGNORE"] [(toString globalGitignore)] (lib.fileContents ./gitconfig);
+  gitConfig = pkgs.writeText "gitconfig" gitConfigContent;
 in {
   wrappers.git = {
     # basePackage = inputs'.git-args.packages.default.override {
@@ -16,11 +20,11 @@ in {
       pkgs.git-lfs
       myGit
     ];
-    env.GIT_CONFIG_GLOBAL.value = ./gitconfig;
+    env.GIT_CONFIG_GLOBAL.value = toString gitConfig;
     env.GIT_CLONE_FLAGS.value = "--recursive --filter=blob:none";
     # postBuild = ''
     #   # export PATH=$out/bin/git:$PATH;
-    #   $out/bin/git-lfs install
+    #   # $out/bin/git-lfs install
     # '';
   };
 }
