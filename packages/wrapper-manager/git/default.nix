5b87dfcdf4e7a2e32d6a3f31b37dd7bd2a467eaf
Jeffrey Li <jeffrey.dot.li@gmail.com>
2026-07-07 09:56:01 -0400

    fix: keep only 10 generations + gitconfig dont force git on linux

--- packages/wrapper-manager/git/default.nix
+++ packages/wrapper-manager/git/default.nix
@@ -6,8 +6,12 @@
 }: let
   myGit = pkgs.gitFull;
   globalGitignore = pkgs.writeText ".gitignore_global" (lib.fileContents ./.gitignore_global);
+  # macOS GUI apps often don't inherit $PATH, so ssh can't be found unless given
+  # an absolute path there. NixOS has no /usr/bin/ssh, so leave it unset there
+  # and let git resolve `ssh` from $PATH instead.
+  sshCommandLine = lib.optionalString pkgs.stdenv.isDarwin "sshCommand = /usr/bin/ssh";
   gitConfigContent =
-    lib.strings.replaceStrings ["$GLOBAL_GITIGNORE"] [(toString globalGitignore)] (lib.fileContents ./gitconfig);
+    lib.strings.replaceStrings ["$GLOBAL_GITIGNORE" "$SSH_COMMAND_LINE"] [(toString globalGitignore) sshCommandLine] (lib.fileContents ./gitconfig);
   gitConfig = pkgs.writeText "gitconfig" gitConfigContent;
 in {
   wrappers.git = {
